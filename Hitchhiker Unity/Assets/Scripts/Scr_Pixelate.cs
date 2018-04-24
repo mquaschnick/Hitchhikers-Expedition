7e97e6456cbca0192e67d1551a8341382eb99dbb
Matthew Quaschnick <mrq27@drexel.edu>
2018-04-23 22:44:20 -0400

    Added the Pixelate Shader, Script, and Material as well as their folders.  Added test scene for shaders

---
+++ Hitchhiker Unity/Assets/Scripts/Scr_Pixelate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode()]
+public class Scr_Pixelate : MonoBehaviour {
+
+
+    public Material effect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // source is all of the pixels coming in, destination is the material we're applying to
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        // Blit runs this shit through the shader
+        // Shaders go through each pixel, and say run this function on this pixel
+        Graphics.Blit(source, destination, effect);
+    }
+}
