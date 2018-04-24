25e9b5d10b4e8edf1ea44336316e287cda3029fb
Matthew Quaschnick <mrq27@drexel.edu>
2018-04-23 21:46:53 -0400

    Stable build of Unity Project, moved gitignore into Unity Project

---
+++ Hitchhiker Unity/Assets/Scripts/Hitchhike.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hitchhike : MonoBehaviour {
+
+	// Public Variables
+	public Slider weather;
+	public Slider population;
+	public Text percent;
+	public Text success;
+
+	// Private Variables
+	private float percent_value;
+
+	void Update () {
+		float weather_chance = weather.value;
+		float population_chance = population.value;
+		percent_value = calculate_chance(weather_chance, population_chance) * 100.0f;
+		percent.text = percent_value + "%";
+	}
+
+	// Calculate Hitchhike success chance
+	float calculate_chance (float weather_chance, float population_chance) {
+		float chance = weather_chance * population_chance;
+		return chance;
+	}
+
+	// See if the Hitchhike is successful
+	public void success_or_fail() {
+		float chance = Random.Range(0.0f, 100.0f);
+		if (chance <= percent_value) {
+			success.text = "Success!!";
+		} else {
+			success.text = "Fail :(";
+		}
+	}
+}
---
+++ Hitchhiker Unity/Assets/Scripts/Scr_Test_Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Test_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3 (transform.position.x, Mathf.Sin (Time.time), transform.position.z);
+		
+	}
+}
