35227a115136ee89bbb0296fa1f680917e8abe34
Matthew Quaschnick <mrq27@drexel.edu>
2018-04-26 20:08:42 -0400

    Made 2D array of String fo Inventory

---
+++ Hitchhiker Unity/Assets/Scripts/Scr_Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Inventory : MonoBehaviour {
+
+    public string[][] inventory;
+
+
+	// Use this for initialization
+	void Start () {
+
+        inventory = new string[10][];
+
+        for (int i = 0; i < inventory.Length; i++) {
+            inventory[i] = new string[10];
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+}
