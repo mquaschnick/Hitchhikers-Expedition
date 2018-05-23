3f37b128833c5bf36bf55cd076b64f5a8756be0d
Matthew Quaschnick <mrq27@drexel.edu>
2018-05-23 18:40:58 -0400

    Added in damage and death from puddles

--- Hitchhiker Unity/Assets/Scripts/PuddleTrigger.cs
+++ Hitchhiker Unity/Assets/Scripts/PuddleTrigger.cs
@@ -6,12 +6,15 @@ public class PuddleTrigger : MonoBehaviour {
 
     public ParticleSystem splashParticles;
     private AudioSource splashAudio;
+    private PlayerController_Statuses playerStatus;
 
     private float splashRange = 2f;
 
 	// Use this for initialization
 	void Start () {
         splashAudio = GetComponent<AudioSource>();
+        playerStatus = PlayerController_Master.playerBody.transform.parent.GetComponent<PlayerController_Statuses>();
+
 	}
 
     void OnTriggerEnter(Collider other)
@@ -21,15 +24,22 @@ public class PuddleTrigger : MonoBehaviour {
             splashParticles.Emit(50);
             splashAudio.Play();
 
-            //Detect if player is within range apply GetWet function
+                        //Detect if player is within range apply GetWet function
             //Something like this depending on how we handle static player to get access to the player position and 
 
             /*
              if (splashRange > Mathf.Abs (Player.instance.gameObject.transform.position.x - transform.position.x)
                 PlayerInventory.instance.WetClothes();
              */
+
+            float distanceFromPlayer = PlayerController_Master.playerBody.transform.position.x - transform.position.x;
+
+            if(Mathf.Abs(distanceFromPlayer) <= splashRange && Mathf.Abs(distanceFromPlayer) >= 0) {
+                playerStatus.damageDeath(-0.1f);
+            }
           
         }
 
     }
+
 }
