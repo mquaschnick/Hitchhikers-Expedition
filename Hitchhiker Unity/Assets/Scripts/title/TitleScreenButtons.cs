234698d19496e3b2c746df6a4130541405a7d814
Mason Griff <masonalexgriff@gmail.com>
2018-06-01 00:48:38 -0400

    Redone HUD and Working Title Screen

---
+++ Hitchhiker Unity/Assets/Scripts/title/TitleScreenButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenButtons : MonoBehaviour {
+
+    [Tooltip("The scene where the game begins, or maybe the scene where you slect Male/Female.")]
+    public string SceneName;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
