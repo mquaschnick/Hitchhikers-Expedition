530266f43145cf8af0f29022a8b4cf8965c27295
Mason Griff <masonalexgriff@gmail.com>
2018-06-09 17:59:51 -0400

    TitleScreen and Death Screen Music added, Success Screen Updated
    Success screen has a working button that takes you back to title screen or resets gameplay scene at touch of a bool. Title Screen and death screen music composed and added

---
+++ Hitchhiker Unity/Assets/Scripts/SuccessScreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SuccessScreen : MonoBehaviour {
+
+    [Tooltip("Set this to on if you don't want the button to reset player back to the level. If you set to on, be sure to type the name of the scene in the string variable below and the script will take care of the rest for you.")]
+    public bool ResetLevelInsteadOfReturnToTitle = false;
+    [Tooltip("If you set the bool above to true, enter the name of the scene for the level here.")]
+    public string LevelName = "BetaEnvironment";
+
+    string ChangeButtonText = "Play Again";
+
+    Text ButtonText;
+
+	// Use this for initialization
+	void Start () {
+        ButtonText = GameObject.Find("Canvas").transform.Find("ResetButton").transform.Find("Text").gameObject.GetComponent<Text>();
+        if (ResetLevelInsteadOfReturnToTitle == true)
+        {
+            ButtonText.text = ChangeButtonText;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ReturnButtonPressed()
+    {
+        if (ResetLevelInsteadOfReturnToTitle == false)
+        {
+            SceneManager.LoadScene("TitleScreen");
+        }
+        else
+        {
+
+            SceneManager.LoadScene(LevelName);
+        }
+    }
+}
